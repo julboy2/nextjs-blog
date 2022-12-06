0e19e55fdf07dd2192910f5dd28d11414107302a
JamesWoo <JamesWoo>
2022-12-06 20:30:19 +0900

    ssg ssr csr 비교 데이터 가져오기

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,39 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import Layout, { siteTitle } from "../components/layout";
+//import { getSortedPostsData } from "../lib/posts";
 import utilStyles from "../styles/utils.module.css";
 
+// ssg 방법
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData();
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
+
+// ssr 방법
+// export async function getServerSideProps() {
+//   const allPostsData = getSortedPostsData();
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
+
 export default function Home() {
+  // csr 방법 시작 : Home 에서 { allPostsData } 삭제해준다.
+  const [allPostsData, setAllPostsData] = useState([]);
+  useEffect(() => {
+    fetch("/api/posts")
+      .then((res) => res.json())
+      .then((data) => setAllPostsData(data.allPostsData));
+  }, []);
+  // csr 방법 끝
+
   return (
     <Layout home>
       <Head>
@@ -16,6 +47,17 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <ul className={utilStyles.list}>
+        {allPostsData.map(({ id, date, title }) => (
+          <li className={utilStyles.listItem} key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 }
