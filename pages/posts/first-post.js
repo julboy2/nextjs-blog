d881c515d9ba99c7f348c9a0325eaa9eba1732a6
huk1183 <huk1183@getplus.co.kr>
2022-12-06 12:49:21 +0900

    css 적용

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Layout from "../../components/Layout";
+
+export default function FirstPost(){
+    return (
+    <Layout>
+    <Head>
+        <title>첫번째 글 타이틀</title>
+    </Head>
+        <h1>첫번째글</h1>
+    </Layout>
+    )
+}
